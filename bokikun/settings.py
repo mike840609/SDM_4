6d313a4e010c58a5d2b9bb3650db4c8189eed7db
DESKTOP-BNH19L5\ianchen <mike840609@gmail.com>
2017-11-29 16:29:41 +0800

    add header

--- bokikun/settings.py
+++ bokikun/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['140.112.107.237','localhost','127.0.0.1' ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    
+    'corsheaders',
 
     #  pip install
     'rest_framework',
@@ -53,6 +58,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 AUTH_USER_MODEL = 'account.User'
